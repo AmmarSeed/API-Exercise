628899cb656898011cdd25798bef1dd31b0ae11e
Hossam <hossameldin.baioumy@powercoders.org>
2022-05-27 14:16:44 +0200

    Css Edits

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
+let main = document.getElementById("main-content");
+let input = document.getElementById("search-input");
+let button = document.getElementById("search");
 
-
-let main=document.getElementById("main-content")
-let input=document.getElementById("search-input")
-let button=document.getElementById("search")
-
-button.addEventListener("click",function () {
-let valueofInput=input.value
-console.log(valueofInput)
-    fetch(`https://newsapi.org/v2/everything?q=${valueofInput}&apiKey=7119599c9b49490fa6ebdbb13c57364d`)
+button.addEventListener("click", function () {
+  let valueofInput = input.value;
+  console.log(valueofInput);
+  fetch(
+    `https://newsapi.org/v2/everything?q=${valueofInput}&apiKey=7119599c9b49490fa6ebdbb13c57364d`
+  )
     .then(response => response.json())
-    .then(data => {console.log(data)
-        let list=data.articles
-        let result=list.map(function (listing) {
-            return `<p> <span>  ${listing.title} <span> </p>
-            <p> ${listing.author} </p>
-            <p>${listing.description}</p>`
-        });
-    main.innerHTML=result
-    })
-})
+    .then(data => {
+      console.log(data);
+      let list = data.articles;
+      let result = list.map(function (listing) {
+        return `<p> <span class ="title">  ${listing.title} <span> </p>
+            <p class = "author"> ${listing.author} </p>
+            <p>${listing.description}</p>`;
+      });
+      main.innerHTML = result;
+    });
+});
